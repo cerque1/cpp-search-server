40b32275f358c7190bbf39b52d41b9a38444300f
Фёдор Рудин <fedor20060609@mail.ru>
2024-01-06 01:43:42 +0300

    Поисковой сервер. Спринт 1. Переименовал переменную, вывел IDF в отдельную функцию

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -61,9 +61,9 @@ public:
     void AddDocument(int document_id, const string& document) {
         document_count_++;
         const vector<string> words = SplitIntoWordsNoStop(document);
-        double tf = 1. / words.size();
+        double word_share_in_document = 1. / words.size();
         for (const string& word : words)
-            word_to_documents_freqs_[word][document_id] += tf;
+            word_to_documents_freqs_[word][document_id] += word_share_in_document;
     }
 
     vector<Document> FindTopDocuments(const string& raw_query) const {
@@ -117,11 +117,16 @@ private:
         return query;
     }
 
+    double ComputeIDF(const string& word) const{
+        return log(document_count_ / static_cast<double>(word_to_documents_freqs_.at(word).size()));
+    }
+
     map<int, double> DocumentToRelevance(const set<string>& query_words) const {
         map<int, double> document_to_relevance;
+
         for (const string& query_word : query_words) {
             if (word_to_documents_freqs_.count(query_word)) {
-                double idf = log(document_count_ / static_cast<double>(word_to_documents_freqs_.at(query_word).size()));
+                double idf = ComputeIDF(query_word);
                 for (const auto& [document_id, tf] : word_to_documents_freqs_.at(query_word))
                     document_to_relevance[document_id] += idf * tf;
             }
